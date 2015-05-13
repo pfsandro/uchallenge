9ba129e051ae14e8d629a1d0e654711acf787750
Luiz Souza <lpsouza@gmail.com>
2015-05-13 20:31:25 -0300

    Alteração no model dos desafios para fazer join na tabela de tema

--- application/models/Desafio_model.php
+++ application/models/Desafio_model.php
@@ -20,8 +20,9 @@ class Desafio_model extends CI_Model {
     function listar() {
         $this->db->where('tipo', 'D');
         $this->db->or_where('tipo', 'P');
+        $this->db->from('acoes');
         $this->db->join('tema', 'acoes.tema_id = tema.id');
-        $query = $this->db->get('acoes');
+        $query = $this->db->get();
         var_dump($query->result());
         return $query->result();
 
